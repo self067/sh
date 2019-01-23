64b1a8165d2f5c16090f7670b7bd9d79e4fe9528
seltor <seltor@mail.ru>
2019-01-24 00:40:42 +0300

    20190123

--- views/category/index.php
+++ views/category/index.php
@@ -11,12 +11,10 @@ $this->title = Yii::$app->name . ' Магазин '; // . $model->name;
         <section class="products">
             <?php foreach( $products as $product) { ?>
             <article>
-                <img src="img/<?=$product['img']?>" alt="<?=$product['name']?>">
+                <img height=210  src="img/<?=$product['img']?>" alt="<?=$product['name']?>">
                 <h3><?=$product['name']?></h3>
                 <h4>$<?=$product['price']?></h4>
-                <a href="cart.html" data-name="<?=$product['link_name']?>" class="btn-add">Add to cart</a>
-<!--                <a href="--><?//=Url::to(['product/index', 'name'=> $product['link_name']] )?><!--" type="button" class="product-button__more btn btn-primary">Подробнее</a>-->
-
+                <a href="<?=Url::to(['product/index', 'name'=> $product['link_name']] )?>" data-name="<?=$product['link_name']?>" class="btn-add">Add to cart</a>
             </article>
             <?php } ?>
 
